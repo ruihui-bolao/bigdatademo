71654b2662e6947ed03f56a93d8e0eb0b10bd163
chengruihui <chengruihui@nowledgedata.com.cn>
2018-02-12 11:06:18 +0800

    利用 spark 从 hive 数据表中读取数据。

--- sparkhive/src/main/java/com/hui/hive/HiveUtils.java
+++ sparkhive/src/main/java/com/hui/hive/HiveUtils.java
@@ -1,9 +1,20 @@
+package com.hui.hive;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.hive.HiveContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.hadoop.hive.HiveTemplate;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,12 +26,19 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  * Description:   实现将 hdfs 上的文件加载到 hive 中。
  */
-public class HiveUtils {
+public class HiveUtils implements Serializable {
 
     /**
-     *  hive use database shell 语句
+     * hive use database shell 语句
      */
     public String useHiveDB = "";
+
+    /**
+     * spark 的上下文
+     */
+    public transient JavaSparkContext sparkContext;
+
+    public static transient HiveContext hiveContext = null;
 
     /**
      * hiveTemplate
@@ -38,10 +56,52 @@ public class HiveUtils {
     /**
      * 初始化使用数据库的语句
      *
-     * @param dataBaseName   hive 中执行操作的数据库
+     * @param dataBaseName hive 中执行操作的数据库
+     * @param sparkContext  spark 上下文机制
      */
-    public HiveUtils(String dataBaseName) {
-        useHiveDB = "use " + dataBaseName + ";";
+    public HiveUtils(String dataBaseName, JavaSparkContext sparkContext) {
+        this.useHiveDB = "use " + dataBaseName + ";";
+        this.sparkContext = sparkContext;
+    }
+
+    /**
+     * 从hive中查询数据,
+     */
+    public JavaRDD<String> getRddData(String hiveDbName , String sqlText) {
+
+        if (hiveContext == null){
+            hiveContext = new HiveContext(sparkContext.sc());
+        }else if (hiveContext.sparkContext() != sparkContext.sc()){
+            hiveContext = null;
+            hiveContext = new HiveContext(sparkContext.sc());
+        }
+
+        hiveContext.sql("use " + hiveDbName);
+        DataFrame dataFrame = hiveContext.sql(sqlText);
+        JavaRDD<String> javaRDD = dataFrame.toJavaRDD().flatMap(new FlatMapFunction<Row, String>() {
+            @Override
+            public Iterable<String> call(Row row) throws Exception {
+                int length = row.length();
+                ArrayList<String> list = new ArrayList<String>(length);
+                for (int i = 0; i < length; ++i) {
+                    String rowTemp = row.getString(i);
+                    JSONObject jsonObject = null;
+                    if (rowTemp != null) {
+                        try {
+                            jsonObject = JSON.parseObject(rowTemp);
+                        } catch (Exception var8) {
+                            continue;
+                        }
+
+                        if (jsonObject != null) {
+                            list.add(jsonObject.toJSONString());
+                        }
+                    }
+                }
+                return list;
+            }
+        });
+        return javaRDD;
     }
 
 
@@ -73,7 +133,7 @@ public class HiveUtils {
             String projectId = jsonObject.getString("projectId");
             String batchId = jsonObject.getString("batchId");
 
-            // 如果不存在先创建分区
+            // 判断该数据的分区是否存在，如果不存在先创建分区
             if (!onlinePartitions.contains(projectId + batchId)) {
                 String addPartition = useHiveDB + "alter table " + tableName + " add partition(projectId='" + projectId + "',batchId='" + batchId + "')";
                 System.out.println("创建的分区为：" + addPartition);
@@ -91,17 +151,17 @@ public class HiveUtils {
         return res;
     }
 
-    /**
-     *  从hive中查询数据
-     */
-    public void queryData(){
-
-    }
 
     public static void main(String[] args) {
 
+        //初始化配置文件
         String dataBase = "test";
-        HiveUtils hiveUtils = new HiveUtils(dataBase);
+        SparkConf conf = new SparkConf();
+        conf.setMaster("local").setAppName("sparkHive");
+        JavaSparkContext sparkContext = new JavaSparkContext(conf);
+        HiveUtils hiveUtils = new HiveUtils(dataBase , sparkContext);
+
+/*        // 1, 测试将 hdfs 上的文件导入到 hive 数据表中。
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("projectId", "testProject");
         jsonObject.put("batchId", "testBatch");
@@ -109,7 +169,18 @@ public class HiveUtils {
         String tableName = "hivetest";
         String filePath = "/data/etl2/origindata/103/881a4456455544568985a13a467acb8a";
         boolean res = hiveUtils.loadData2Hive(data, tableName, filePath);
-        System.out.println(res);
-    }
+        System.out.println(res);*/
 
+        //2，测试利用 spark 读取 hive 数据表。
+        String hiveTableName = "hivetest";
+        String projectId = "testProject";
+        String batchId = "testBatch";
+        String sqlString = "select jsondata from  " + hiveTableName + "  where  projectId=  '" + projectId + "'  and batchId= '" + batchId + "'";
+        JavaRDD<String> rddData = hiveUtils.getRddData(dataBase, sqlString);
+        List<String> collect = rddData.collect();
+        for (String s : collect) {
+            System.out.println(s);
+        }
+
+    }
 }
