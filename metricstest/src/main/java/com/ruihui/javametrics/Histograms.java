60af546430cd055ccb440225986484191a25d30c
chengruihui <chengruihui@nowledgedata.com.cn>
2018-05-18 09:39:29 +0800

    java metric 监控测试

---
+++ metricstest/src/main/java/com/ruihui/javametrics/Histograms.java
@@ -0,0 +1,40 @@
+package com.ruihui.javametrics;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.ExponentiallyDecayingReservoir;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bolao
+ * Date: 2018/5/17 15:54
+ * Version: V1.0
+ * To change this template use File | Settings | File Templates.
+ * Description:  Histogram统计数据的分布情况。比如最小值，最大值，中间值，还有中位数，75百分位, 90百分位, 95百分位, 98百分位,
+ * 99百分位, 和 99.9百分位的值(percentiles)。
+ */
+public class Histograms {
+
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        MetricRegistry registry = new MetricRegistry();
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(registry).build();
+        reporter.start(1, TimeUnit.SECONDS);
+        Histogram histogram = new Histogram(new ExponentiallyDecayingReservoir());
+        registry.register(MetricRegistry.name(Histograms.class,"request","history"),histogram);
+        while (true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            histogram.update(random.nextInt(1000));
+        }
+    }
+
+}
---
+++ metricstest/src/main/java/com/ruihui/javametrics/TimerTest.java
@@ -0,0 +1,40 @@
+package com.ruihui.javametrics;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bolao
+ * Date: 2018/5/17 16:03
+ * Version: V1.0
+ * To change this template use File | Settings | File Templates.
+ * Description:  Timer其实是 Histogram 和 Meter 的结合
+ */
+public class TimerTest {
+
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        MetricRegistry registry = new MetricRegistry();
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(registry).build();
+        reporter.start(1, TimeUnit.SECONDS);
+        Timer timer = registry.timer(MetricRegistry.name(TimerTest.class, "get"));
+        Timer.Context ctx;
+        while (true){
+            ctx = timer.time();
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ctx.stop();
+        }
+    }
+
+
+}
