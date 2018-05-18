73e2322dff66fe0ba01709407b10f48d4dc3bb63
chengruihui <chengruihui@nowledgedata.com.cn>
2018-05-18 09:40:53 +0800

    java metric 监控测试

---
+++ metricstest/src/main/java/com/ruihui/metricstest/MetricstestApplication.java
@@ -0,0 +1,12 @@
+package com.ruihui.metricstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricstestApplication.class, args);
+	}
+}
