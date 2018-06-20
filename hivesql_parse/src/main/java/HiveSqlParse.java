7cfc51b17456a73ef39014bef8f992127e69c9cb
chengruihui <chengruihui@nowledgedata.com.cn>
2018-06-20 08:57:44 +0800

    使用hiveSql解析sql语句

--- hivesql_parse/src/main/java/HiveSqlParse.java
+++ hivesql_parse/src/main/java/HiveSqlParse.java
@@ -1,7 +1,13 @@
+import org.antlr.runtime.tree.Tree;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.lib.Node;
 import org.apache.hadoop.hive.ql.parse.ASTNode;
 import org.apache.hadoop.hive.ql.parse.ParseDriver;
 import org.apache.hadoop.hive.ql.Context;
+import org.apache.hadoop.hive.ql.parse.ParseException;
+
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,12 +19,55 @@ import org.apache.hadoop.hive.ql.Context;
  */
 public class HiveSqlParse {
 
+
+    /**
+     * hive 解析sql语句返回的是树
+     *
+     * @param sqlStr
+     * @return
+     */
+    public ASTNode hiveParseSql(String sqlStr) {
+        ASTNode node = null;
+        try {
+            ParseDriver pd = new ParseDriver();
+            node = pd.parse(sqlStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return node;
+    }
+
+    /**
+     * 对hive解析的树进行解析
+     *
+     * @param node
+     */
+    public String parseTree(ASTNode node) {
+        int childCount = node.getChildCount();
+        // 解析返回的结果
+        String resStr = null;
+        //  用来判断是否有空
+        if (childCount != 0) {
+            for (int i = 0; i < childCount; i++) {
+                Tree child = node.getChild(i);
+                if (child.getChildCount() == 0 && child.getTokenStopIndex() != -1) {
+                    System.out.println(child.getText());
+                    return child.getText();
+                }
+                resStr = parseTree((ASTNode) child);
+                if (StringUtils.isNotBlank(resStr)) {
+                    System.out.println(child.getText());
+                }
+            }
+        }
+        return resStr;
+    }
+
     public static void main(String[] args) throws Exception {
         String sql = "select * from table1 where a > 100";
-//        Context context = new Context(new HiveConf());
-        ParseDriver pd = new ParseDriver();
-        ASTNode node = pd.parse(sql);
-        System.out.println(node);
+        HiveSqlParse hiveSqlParse = new HiveSqlParse();
+        ASTNode node = hiveSqlParse.hiveParseSql(sql);
+        hiveSqlParse.parseTree(node);
     }
 
 }
